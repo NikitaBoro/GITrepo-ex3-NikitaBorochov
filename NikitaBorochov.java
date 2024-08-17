0542a913c6ad4c15f94f80e80d573ed86c30326e
NikitaBoro <nikitaboro03@gmail.com>
2024-08-17 11:55:21 +0300

    Modified NikitaBorochov.java

--- NikitaBorochov.java
+++ NikitaBorochov.java
@@ -1,7 +1,7 @@
 public class NikitaBorochov{
     public static void main(String[] args){
         for (int i=1;i<=5;i++){
-            System.out.println("some different print "+i);
+            System.out.println("jon 3 "+i);
         }
     }
 }
