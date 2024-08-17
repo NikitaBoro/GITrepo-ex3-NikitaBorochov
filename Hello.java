ad17029b45c368db8461c442fa703debb711c3c0
NikitaBoro <nikitaboro03@gmail.com>
2024-08-17 11:58:00 +0300

    Modified Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         for (int i=1;i<=10;i++){
-            System.out.println("job 1 "+i);
+            System.out.println("Not Hello World "+i);
         }
     }
 }
